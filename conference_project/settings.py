4317fb7cd00addf05f140ac51b4003c0374f8f6e
12yasaswi56 <139425353+12yasaswi56@users.noreply.github.com>
2025-04-29 11:41:43 +0530

    Update settings.py

--- conference_project/settings.py
+++ conference_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vpzy&vjbvsp=!1292bpo1b4rt_x!+(&u)p+rsrs_ks-q_*umuv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
